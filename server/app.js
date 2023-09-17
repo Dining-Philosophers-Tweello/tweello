0c2311de9611061dd3d3bf218cdee9d7cbef483e
sarahzacky <szacky10@gmail.com>
2023-09-17 16:01:11 -0700

    Connects MongoDB but has issue with reading .env variables

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-
 dotenv.config();
 
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+
+
 const port = 3000;
+
+connectDB(); // this is the line that is causing the crash
+
 const app = express();
 
 app.use(express.json());
--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGO URI IS", process.env.MONGO_URI);
+        //const uri = provess.env.MONGO_URI;
+        const uri = "mongodb+srv://tweelloDB:3r0MbFph6HaUU8hv@tweello.qnufuue.mongodb.net/?retryWrites=true&w=majority";
+        //const conn = await mongoose.connect(process.env.MONGO_URI);        
+        const conn = await mongoose.connect(uri);   
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
