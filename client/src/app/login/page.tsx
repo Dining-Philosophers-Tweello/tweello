0dc0f56e92365027c8f621b15cbac60e2706c47c
Jordan Hilado <jordanalihilado@gmail.com>
2023-09-18 18:26:43 -0700

    login page frontend done

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 
-import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "./user-auth-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -14,64 +12,36 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <Button>test</Button>
-      {/* <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col items-center border border-gray-300 rounded-2xl w-96 h-96 p-5 bg-gray-50">
+          <div className="text-3xl p-6">Sign In</div>
+          <Input
+            className="m-2"
+            id="email"
+            placeholder="Enter your email"
+            type="email"
+            autoCapitalize="none"
+            autoComplete="email"
+            autoCorrect="off"
+          />
+          <Input
+            id="password"
+            className="m-2"
+            placeholder="Enter your password"
+            type="password"
+            autoCapitalize="none"
+            autoComplete="password"
+            autoCorrect="off"
+          />
+          <Button className="w-full m-2">Sign In</Button>
+          <div className="m-2">
+            Don't have an account?{" "}
+            <Link href="/" className="text-gray-500 underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Sign In
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Tweello
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
-            </div>
-            <UserAuthForm />
-            <div className="w-full border border0-r">
-              Don't have an account?
-              <Link href="/">Home</Link>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
