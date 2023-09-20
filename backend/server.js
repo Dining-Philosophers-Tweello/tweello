b4fc4d31a3257b77da57ddd41aeac4cdef5ac350
sarahzacky <szacky10@gmail.com>
2023-09-19 18:42:07 -0700

    Consolidating files
    Moving files that I previously worked on from "backend" folder into "server" folder

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'; 
-import userRoutes from './routes/userRoutes.js'
-
-app.use('/api/users', userRoutes);
-app.use(notFound);
-app.use(errorHandler);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-const router = express.Router();
 import { authUser } from '../controllers/userController.js';
 
+const router = express.Router(); 
 router.post('/auth', authUser);
 
 export default router;
