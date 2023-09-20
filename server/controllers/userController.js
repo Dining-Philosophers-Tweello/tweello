09a02860e81c501af3ff49d67a2496a044989260
Grant Smith <grantsmith01@verizon.net>
2023-09-20 13:53:10 -0700

    Create endpoint for authenticating users (login)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,12 @@
 import asyncHandler from 'express-async-handler'
 
+// @desc    Authenticate (login) a user, set token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (request, response) => {
+    response.status(200).json({ message: 'User authentication successful' });
+});
+
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
@@ -7,4 +14,4 @@ const registerUser = asyncHandler(async (request, response) => {
     response.status(200).json({message: 'User registration successful'});
 });
 
-export {registerUser};
+export {authUser, registerUser};
