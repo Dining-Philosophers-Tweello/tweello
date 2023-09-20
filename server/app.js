ec5edcff63086db72581913bfefec274ceb9e6ba
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2023-09-20 15:37:36 -0700

    Added prettier, formatted code

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import userRoutes from './routes/userRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT;
 connectDB();
 const app = express();
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Server is ready!')
+app.get("/", (req, res) => {
+  res.send("Server is ready!");
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
