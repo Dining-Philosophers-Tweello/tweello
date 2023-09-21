2e2aed01748f017427cb2d649e2807ab308bee14
sarahzacky <szacky10@gmail.com>
2023-09-20 19:30:47 -0700

    Adding name/email/password to Register User endpoint
    Also throw an error if the user already exists

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js';
 
 // @desc    Authenticate (login) a user, set token
 // @route   POST /api/users/auth
@@ -11,7 +12,14 @@ const authUser = asyncHandler(async (request, response) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (request, response) => {
-    response.status(200).json({message: 'User registration successful'});
+    const {name, email, password} = request.body;
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        response.status(400);
+        throw new Error('This user is already registered');
+      }
+
+      const user = await User.create({name, email, password,});
 });
 
 export {authUser, registerUser};
