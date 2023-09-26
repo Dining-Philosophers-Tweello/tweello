970cae35c524452d046f7dc8a268cc8857ca21b0
Jordan Hilado <jordanalihilado@gmail.com>
2023-09-26 16:53:37 -0700

    move Link tag to child component

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,15 +1,13 @@
 import { BoardCard } from "@/components/board-card";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="p-5">
-      <Link href="/">
-        <BoardCard
-          title="Application Development"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-      </Link>
+      <BoardCard
+        link="/"
+        title="Application Development"
+        description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
     </div>
   );
 }
