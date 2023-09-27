c0e7c765f0337b49ce83f5ed1775adb7308f8022
Jordan Hilado <jordanalihilado@gmail.com>
2023-09-26 17:52:10 -0700

    form homepage screen with board components

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,13 +1,86 @@
 import { BoardCard } from "@/components/board-card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface BoardCardData {
+  link: string;
+  title: string;
+  description: string;
+}
+
+const boardsData: BoardCardData[] = [
+  {
+    link: "/",
+    title: "Project A",
+    description:
+      "Ut enim ad minim veniam, exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    link: "/",
+    title: "Project B",
+    description:
+      "Lorem ipsum dolor sit amet, ut labore et dolore magna aliqua.",
+  },
+  {
+    link: "/",
+    title: "Project B",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci, sed do eiusmod tempor incididunt ut.",
+  },
+  {
+    link: "/",
+    title: "Project D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    link: "/",
+    title: "Project E",
+    description:
+      "Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    link: "/",
+    title: "Project F",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="p-5">
-      <BoardCard
-        link="/"
-        title="Application Development"
-        description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-      />
+    <div className="flex flex-col gap-5 p-5 w-screen h-screen">
+      <div className="flex flex-row gap-5">
+        <div className="text-3xl">Workspace 1 Boards</div>
+        <Dialog>
+          <DialogTrigger>
+            <div className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
+              Create Board
+            </div>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create new board</DialogTitle>
+              <DialogDescription>Create new board</DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="flex gap-5 flex-wrap">
+        {boardsData.map((board) => (
+          <BoardCard
+            key={board.title}
+            link={board.link}
+            title={board.title}
+            description={board.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/components/board-card.tsx
+++ client/src/components/board-card.tsx
@@ -8,14 +8,28 @@ interface BoardCardProps {
 }
 
 export const BoardCard = ({ title, description, link }: BoardCardProps) => {
+  const truncateString = (inputString: string): string => {
+    const maxLength = 132;
+    if (inputString.length <= maxLength) {
+      return inputString;
+    } else {
+      return inputString.slice(0, maxLength - 3) + "...";
+    }
+  };
+
+  const displayDesc = truncateString(description);
+
   return (
-    <Link href={link}>
-      <div className="group flex flex-col bg-sky-100 w-96 h-fit-content rounded-lg gap-3 p-5 hover:shadow-xl transition-shadow duration-300">
+    <Link
+      className="flex flex-col bg-sky-100 w-96 h-56 rounded-lg gap-3 p-5 hover:shadow-xl transition-shadow duration-300 justify-between"
+      href={link}
+    >
+      <div className="flex flex-col gap-3">
         <div className="text-xl">{title}</div>
-        <div className="font-light text-gray-500">{description}</div>
-        <div className="flex justify-end">
-          <ArrowRight className="text-gray-400" size={24} />
-        </div>
+        <div className="font-light text-gray-500">{displayDesc}</div>
+      </div>
+      <div className="flex justify-end">
+        <ArrowRight className="text-gray-400" size={24} />
       </div>
     </Link>
   );
