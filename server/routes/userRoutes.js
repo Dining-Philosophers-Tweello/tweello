c9b13f2179f05674add3423d5ea8ac7dd857cf1d
Grant Smith <grantsmith01@verizon.net>
2023-10-04 14:44:47 -0700

    Implemented logout user API endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,17 @@
 import express from "express";
 import {
   authUser,
+  logoutUser,
+  registerUser,
   deleteUser,
   editUserProfile,
-  registerUser,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
 router.route("/:id").delete(deleteUser);
 //TODO: Add get(getUserProfile). before .put on below line once getUserProfile implemented
 router.route("/:id").put(editUserProfile);
