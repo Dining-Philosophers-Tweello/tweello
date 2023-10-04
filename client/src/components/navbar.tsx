9ec0955b1006209114bbc20a3b807afa6503e00e
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2023-10-04 14:24:53 -0700

    format project

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,28 +1,10 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import WorkspaceDialog from "@/components/workspace-dialog";
 import Link from "next/link";
 import DeleteWorkspaceDialog from "./delete-workspace-dialog";
 import ProfileDialog from "./profile-dialog";
 import ProfileSettingsDialog from "./profile-settings-dialog";
 import ShareWorkspaceDialog from "./share-workspace-dialog";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import WorkspaceDialog from "@/components/workspace-dialog";
 
 const emails = [
   "diego@gmail.com",
@@ -34,37 +16,36 @@ const emails = [
 
 const Navbar = () => {
   return (
-      <nav className="w-full p-6 border-b border-gray-300">
-        <div className="container mx-auto flex justify-between items-center">
-          <ul className="flex justify-center items-center space-x-8">
-            <li>
-              <Link className="text-2xl font-bold" href="/">
-                Tweello
-              </Link>
-            </li>
-            <li>
-              <WorkspaceDialog/>
-            </li>
-            <li>
-              <ShareWorkspaceDialog emails={emails}/>
-            </li>
-            <li>
-              <DeleteWorkspaceDialog/>
-            </li>
-          </ul>
-          <ul className="flex space-x-6">
-            <li>
-              <ProfileSettingsDialog/>
-            </li>
-            <li>
-              <ProfileDialog/>
-            </li>
-            <ModeToggle/>
-          </ul>
-        </div>
-      </nav>
+    <nav className="w-full p-6 border-b border-gray-300">
+      <div className="container mx-auto flex justify-between items-center">
+        <ul className="flex justify-center items-center space-x-8">
+          <li>
+            <Link className="text-2xl font-bold" href="/">
+              Tweello
+            </Link>
+          </li>
+          <li>
+            <WorkspaceDialog />
+          </li>
+          <li>
+            <ShareWorkspaceDialog emails={emails} />
+          </li>
+          <li>
+            <DeleteWorkspaceDialog />
+          </li>
+        </ul>
+        <ul className="flex space-x-6">
+          <li>
+            <ProfileSettingsDialog />
+          </li>
+          <li>
+            <ProfileDialog />
+          </li>
+          <ModeToggle />
+        </ul>
+      </div>
+    </nav>
   );
-
 };
 
 export default Navbar;
--- client/src/components/workspace-dialog.tsx
+++ client/src/components/workspace-dialog.tsx
@@ -1,52 +1,56 @@
-import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function WorkspaceDialog() {
-    return (
-        <Dialog>
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <p className="text-xl">Workspaces</p>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuItem>Workspace 1</DropdownMenuItem>
-                    <DropdownMenuItem>Workspace 2</DropdownMenuItem>
-                    <DropdownMenuItem>Workspace 3</DropdownMenuItem>
-                    <DialogTrigger asChild>
-                        <DropdownMenuItem>Create New Workspace</DropdownMenuItem>
-                    </DialogTrigger>
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Create New Workspace</DialogTitle>
-                    <DialogDescription></DialogDescription>
-                </DialogHeader>
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <p className="text-xl">Workspaces</p>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Workspace 1</DropdownMenuItem>
+          <DropdownMenuItem>Workspace 2</DropdownMenuItem>
+          <DropdownMenuItem>Workspace 3</DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>Create New Workspace</DropdownMenuItem>
+          </DialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Workspace</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
 
-                <div className="grid gap-4 py-4">
-                    <div className="grid  items-center gap-4">
-                        <Label htmlFor="name" className="text-left text-sm">
-                            Workspace Name
-                        </Label>
-                        <Input id="" className="col-span-3"/>
-                    </div>
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Create</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
-
+        <div className="grid gap-4 py-4">
+          <div className="grid  items-center gap-4">
+            <Label htmlFor="name" className="text-left text-sm">
+              Workspace Name
+            </Label>
+            <Input id="" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Create</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
