3c8b8bf971d632e81cf0857901958fb71c4c73a2
Jordan Hilado <jordanalihilado@gmail.com>
2023-10-11 16:06:54 -0700

    create delete workspace controller + route

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createWorkspace } from "../controllers/workspaceController.js";
+import {
+  createWorkspace,
+  deleteWorkspace,
+} from "../controllers/workspaceController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, createWorkspace);
+router.route("/:id").delete(protect, deleteWorkspace);
 
 export default router;
