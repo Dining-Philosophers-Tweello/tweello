d334f42fd3b197ab23a8376d5b94ede438787ba2
Diego O Garcia <diegogar577@gmail.com>
2023-10-11 14:39:15 -0700

    prettier + prettier reinstall

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,17 +1,12 @@
-"use client"
-import Head from 'next/head';
-import {Field, Formik, useFormik} from 'formik';
+"use client";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Formik, useFormik } from "formik";
+import Head from "next/head";
 import Link from "next/link";
-import {string, object, ref} from "yup";
-import {Label} from "@/components/ui/label";
-
-
-
-
-
+import { object, ref, string } from "yup";
 
 export default function Register() {
   type FormValues = {
@@ -19,66 +14,71 @@ export default function Register() {
     email: string;
     password: string;
     confirmPassword: string;
-  }
-  const apiUrl =  "http://localhost:8000/api/users";
+  };
+  const apiUrl = "http://localhost:8000/api/users";
 
   const validationSchema = object({
-    name: string().min(2, 'Name must be at least 2 characters').required('Name is a required field.'),
+    name: string()
+      .min(2, "Name must be at least 2 characters")
+      .required("Name is a required field."),
     // line
-    email: string().email('Email must be a valid email.').required('Email is a required field.'),
+    email: string()
+      .email("Email must be a valid email.")
+      .required("Email is a required field."),
     password: string()
-        .required('Required')
-        .min(6, 'Minimum 6 characters, at least one number, and special character.')
-        .matches(
-            /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/,
-            'Minimum 6 characters, at least one number, and special character.'
-        ),
+      .required("Required")
+      .min(
+        6,
+        "Minimum 6 characters, at least one number, and special character.",
+      )
+      .matches(
+        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/,
+        "Minimum 6 characters, at least one number, and special character.",
+      ),
     confirmPassword: string()
-        .oneOf([ref('password')], 'Passwords must match')
-        .required('Required'),
+      .oneOf([ref("password")], "Passwords must match")
+      .required("Required"),
   });
   const handleSubmit = async (values: FormValues) => {
     try {
-      console.log(JSON.stringify(values))
+      console.log(JSON.stringify(values));
       const response = await fetch(`${apiUrl}`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(values),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
       const data = await response.json();
       console.log(data);
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error: ", error);
     }
-  }
+  };
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     },
     validationSchema: validationSchema,
     onSubmit: async (values: FormValues) => {
       try {
-        console.log(JSON.stringify(values))
+        console.log(JSON.stringify(values));
         const response = await fetch(`${apiUrl}`, {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify(values),
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         });
         const data = await response.json();
         console.log(data);
-      }
-      catch (error) {
+      } catch (error) {
         console.error("Error: ", error);
       }
-    }
+    },
   });
   return (
     <>
@@ -92,103 +92,112 @@ export default function Register() {
             <div className="text-3xl leading-none tracking-tight">Register</div>
           </div>
           <div className="py-6 pt-0 grid gap-3">
-            <Formik initialValues={formik.initialValues}  validationSchema={validationSchema} onSubmit={handleSubmit}>
+            <Formik
+              initialValues={formik.initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
               {(formik) => (
-                  <form className="w-80 grid gap-y-4 transition-all " onSubmit={formik.handleSubmit}>
-                    <div className="grid gap-2">
-                      <Label>First Name</Label>
-                      <Input
-                          id="name"
-                          name="name"
-                          placeholder=""
-                          type="text"
-                          autoCapitalize="none"
-                          autoComplete="name"
-                          autoCorrect="off"
-                          onChange={formik.handleChange}
-                          value={formik.values.name}
-                      />
-                      {formik.touched.name && formik.errors.name ? (
-                          <div className="flex items-center">
-                            <Icons.infocircle className="w-[1.5rem]" />
-                            <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
-                              {formik.errors.name}
-                            </div>
-                          </div>
-                      ) : null}
-                    </div>
-                    <div className="grid gap-2">
-                      <Label>Email</Label>
-                      <Input
-                          className=" "
-                          id="email"
-                          placeholder=""
-                          type="email"
-                          autoCapitalize="none"
-                          autoComplete="email"
-                          autoCorrect="off"
-                          onChange={formik.handleChange}
-                          value={formik.values.email}
-                      />
-                      {formik.touched.email && formik.errors.email ? (
-                          <div className="flex items-center">
-                            <Icons.infocircle className=" w-[1.5rem]" />
-                            <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
-                              {formik.errors.email}
-                            </div>
-                          </div>
-                        ) : null}
-                    </div>
-                    <div className="grid gap-2">
-                      <Label>Password</Label>
-                      <Input
-                          id="password"
-                          name = "password"
-                          className=" "
-                          placeholder=""
-                          type="password"
-                          autoCapitalize="none"
-                          autoComplete="password"
-                          autoCorrect="off"
-                          onChange={formik.handleChange}
-                          value={formik.values.password}
-                      />
-                      {formik.touched.password && formik.errors.password ? (
-                          <div className="flex items-center">
-                            <Icons.infocircle className=" w-[1.5rem]" />
-                            <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
-                              {formik.errors.password}
-                            </div>
-                          </div>
-                      ) : null}
-                    </div>
-                    <div className="grid gap-2">
-                      <Label>Confirm Password</Label>
-                      <Input
-                          id="confirmPassword"
-                          name = "confirmPassword"
-                          className=""
-                          placeholder=""
-                          type="password"
-                          autoCapitalize="none"
-                          autoComplete="new-password"
-                          autoCorrect="off"
-                          onChange={formik.handleChange}
-                          value={formik.values.confirmPassword}
-                      />
-                      {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-                          <div className="flex items-center">
-                            <Icons.infocircle className=" w-[1.5rem]" />
-                            <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
-                              {formik.errors.confirmPassword}
-                            </div>
-                          </div>
-                      ) : null}
-                    </div>
-                    <Button className="w-full  hover:bg-foreground m-2">Register</Button>
-                  </form>
+                <form
+                  className="w-80 grid gap-y-4 transition-all "
+                  onSubmit={formik.handleSubmit}
+                >
+                  <div className="grid gap-2">
+                    <Label>First Name</Label>
+                    <Input
+                      id="name"
+                      name="name"
+                      placeholder=""
+                      type="text"
+                      autoCapitalize="none"
+                      autoComplete="name"
+                      autoCorrect="off"
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                    />
+                    {formik.touched.name && formik.errors.name ? (
+                      <div className="flex items-center">
+                        <Icons.infocircle className="w-[1.5rem]" />
+                        <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
+                          {formik.errors.name}
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className="grid gap-2">
+                    <Label>Email</Label>
+                    <Input
+                      className=" "
+                      id="email"
+                      placeholder=""
+                      type="email"
+                      autoCapitalize="none"
+                      autoComplete="email"
+                      autoCorrect="off"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                      <div className="flex items-center">
+                        <Icons.infocircle className=" w-[1.5rem]" />
+                        <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
+                          {formik.errors.email}
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className="grid gap-2">
+                    <Label>Password</Label>
+                    <Input
+                      id="password"
+                      name="password"
+                      className=" "
+                      placeholder=""
+                      type="password"
+                      autoCapitalize="none"
+                      autoComplete="password"
+                      autoCorrect="off"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                      <div className="flex items-center">
+                        <Icons.infocircle className=" w-[1.5rem]" />
+                        <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
+                          {formik.errors.password}
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className="grid gap-2">
+                    <Label>Confirm Password</Label>
+                    <Input
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      className=""
+                      placeholder=""
+                      type="password"
+                      autoCapitalize="none"
+                      autoComplete="new-password"
+                      autoCorrect="off"
+                      onChange={formik.handleChange}
+                      value={formik.values.confirmPassword}
+                    />
+                    {formik.touched.confirmPassword &&
+                    formik.errors.confirmPassword ? (
+                      <div className="flex items-center">
+                        <Icons.infocircle className=" w-[1.5rem]" />
+                        <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
+                          {formik.errors.confirmPassword}
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
+                  <Button className="w-full  hover:bg-foreground m-2">
+                    Register
+                  </Button>
+                </form>
               )}
-
             </Formik>
           </div>
           <div className="mb-6">
