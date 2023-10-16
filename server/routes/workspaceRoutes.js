725507f6ab3bb0e5c0e4960a60aa953d42e0044a
Grant Smith <grantsmith01@verizon.net>
2023-10-16 14:32:05 -0700

    Created API Endpoint to edit workspace name

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createWorkspace,
   deleteWorkspace,
+  editWorkspace,
   shareWorkspace,
 } from "../controllers/workspaceController.js";
 import { protect } from "../middleware/authMiddleware.js";
@@ -9,6 +10,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, createWorkspace);
+router.route("/:id").put(protect, editWorkspace);
 router.route("/:id").delete(protect, deleteWorkspace);
 router.route("/:id/share").put(protect, shareWorkspace);
 
