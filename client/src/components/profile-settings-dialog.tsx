db1b33efffd54bcd7669ab3da94f12b4b6d8bf49
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2023-10-21 15:02:13 -0700

    integrated get and edit profile

--- client/src/components/profile-settings-dialog.tsx
+++ client/src/components/profile-settings-dialog.tsx
@@ -1,54 +1,32 @@
 "use client";
-
+import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@/components/ui/label";
+import { DialogClose } from "@radix-ui/react-dialog";
 import { Settings } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
-const formSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  currentPassword: z.string(),
-  newPassword: z.string(),
-  confirmNewPassword: z.string(),
-});
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 const ProfileSettingsDialog = () => {
-  const [profile, setProfile] = useState(null);
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      newPassword: "",
-      confirmNewPassword: "",
-    },
+  const [open, setOpen] = useState(false);
+  const [profile, setProfile] = useState({
+    name: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
   });
 
   useEffect(() => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/api/users/profile";
     const jwt = localStorage.getItem("jwt");
-
     const requestOptions = {
       method: "GET",
       headers: {
@@ -57,127 +35,137 @@ const ProfileSettingsDialog = () => {
       },
     };
 
-    fetch(apiUrl, requestOptions)
+    fetch("http://localhost:8000/api/users/profile", requestOptions)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
+
         return response.json();
       })
       .then((data) => {
-        setProfile(data);
-        console.log("Response data:", data);
+        setProfile({ ...profile, name: data.name, email: data.email });
       })
       .catch((error) => {
         console.error("Error fetching profile:", error);
       });
   }, []);
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProfile({ ...profile, [name]: value });
+    validateField(name, value);
+  };
+
+  const validateField = (name: string, value: string) => {
+    let errorMessage = "";
+
+    if (name === "name") {
+      if (value.trim() === "") {
+        errorMessage = "Name is required";
+      }
+    }
+
+    setErrors({ ...errors, [name]: errorMessage });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const { name } = profile;
+    validateField("name", name);
+
+    if (!errors.name) {
+      const body = {
+        ...(profile.name && { name: profile.name }),
+      };
+      const jwt = localStorage.getItem("jwt");
+      const requestOptions = {
+        method: "PUT",
+        body: JSON.stringify(body),
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+          "Content-Type": "application/json",
+        },
+      };
+
+      try {
+        const response = await fetch(
+          "http://localhost:8000/api/users/profile",
+          requestOptions,
+        );
+
+        if (response.ok) {
+          setOpen(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
         <Settings size={40} />
       </DialogTrigger>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Profile Settings</DialogTitle>
-              <div className="pt-[20px]">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="pt-[10px]">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input disabled type="email" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="pt-[10px]">
-                <FormField
-                  control={form.control}
-                  name="currentPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Current Password</FormLabel>
-                      <FormControl>
-                        <Input type="password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="pt-[10px]">
-                <FormField
-                  control={form.control}
-                  name="newPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>New Password</FormLabel>
-                      <FormControl>
-                        <Input type="password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="pt-[10px]">
-                <FormField
-                  control={form.control}
-                  name="confirmNewPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Confirm New Password</FormLabel>
-                      <FormControl>
-                        <Input type="password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </DialogHeader>
-            <DialogFooter className="pt-[20px]">
-              <DialogClose asChild>
-                <Button className="w-1/2 mr-2" variant="secondary">
-                  Cancel
-                </Button>
-              </DialogClose>
-              <DialogClose asChild>
-                <Button className="w-1/2 mr-2" type="submit">
-                  Update
-                </Button>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
+      <DialogContent>
+        <form className="space-y-8" onSubmit={handleSubmit}>
+          <DialogHeader>
+            <DialogTitle>Profile Settings</DialogTitle>
+            <div className="pt-[20px] grid gap-2">
+              <Label>Name</Label>
+              <Input
+                id="name"
+                name="name"
+                placeholder=""
+                type="text"
+                autoCapitalize="none"
+                autoComplete="name"
+                autoCorrect="off"
+                onChange={handleChange}
+                value={profile.name}
+              />
+              {errors.name ? (
+                <div className="flex items-center">
+                  <Icons.infocircle className="w-[1.5rem]" />
+                  <div className="text-[.8rem] text-gray-500 m-2 no-wrap">
+                    {errors.name}
+                  </div>
+                </div>
+              ) : null}
+            </div>
+            <div className="pt-[20px] grid gap-2">
+              <Label>Email</Label>
+              <Input
+                className=" "
+                id="email"
+                placeholder=""
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                value={profile.email}
+                disabled
+              />
+            </div>
+          </DialogHeader>
+          <DialogFooter className="pt-[20px]">
+            <DialogClose asChild>
+              <Button className="w-1/2 mr-2" variant="secondary">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button
+              className="w-1/2 mr-2"
+              type="submit"
+              disabled={errors.name !== ""}
+            >
+              Update
+            </Button>
+          </DialogFooter>
         </form>
-      </Form>
+      </DialogContent>
     </Dialog>
   );
 };
