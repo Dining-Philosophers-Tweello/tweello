2a6ba3e70e75af8d63a58d57c296a7f19b572c46
Diego O Garcia <diegogar577@gmail.com>
2023-10-24 13:16:54 -0700

    Share board removal

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,18 +1,8 @@
-import ShareBoardDialog from "@/components/share-board-dialog";
-
-const emails = [
-  "diego@gmail.com",
-  "jordan@gmail.com",
-  "sarah@gmail.com",
-  "grant@gmail.com",
-  "daniel@gmail.com",
-];
 export default function Board() {
   return (
     <div className="flex flex-col gap-5 p-5 w-screen h-screen">
       <div className="flex flex-row gap-5">
         <div className="text-3xl">Insert board name here</div>
-        <ShareBoardDialog emails={emails} />
       </div>
       <div className="flex gap-5 flex-wrap">Insert Columns Here</div>
     </div>
