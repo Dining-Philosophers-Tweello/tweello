2512a6a0f8bf9d5c56abbcab0fab922b81b67ae7
sarahzacky <szacky10@gmail.com>
2023-10-23 18:59:37 -0700

    Add functionality to createBoard API call

--- server/controllers/boardController.js
+++ server/controllers/boardController.js
@@ -5,35 +5,73 @@ import Board from '../models/boardModel.js';
 // @desc    Create a new board
 // @route   POST /api/workspaces/:workspaceId/boards/:boardId
 // @access  Private
-const createBoard = asyncHandler(async (request, res) => {
-    // To-do
- });
+const createBoard = asyncHandler(async (request, response) => {
+  const { name } = request.body;
+  const creator = request.user._id;
+  const workspaceId = request.params.workspaceId;
+
+  // Check if the user has access to the workspace
+  const workspace = await Workspace.findById(workspaceId);
+
+  if (!workspace) {
+    response.status(404);
+    throw new Error("Workspace not found");
+  }
+
+  if (workspace.creator.toString() !== request.user._id.toString() && !workspace.members.includes(request.user._id)) {
+    response.status(403);
+    throw new Error("Unauthorized access to this workspace");
+  }
+
+  // Check if a board with the same name exists in the workspace
+  const boardExists = await Board.findOne({ name, workspace: workspaceId });
+
+  if (boardExists) {
+    response.status(400);
+    throw new Error("Board with this name already exists in the workspace");
+  }
+
+  // Create a new board in the workspace
+  const board = await Board.create({ name, creator, workspace: workspaceId });
+
+  if (board) {
+    response.status(201).json({
+      _boardId: board._id,
+      name: board.name,
+      creator: board.creator,
+      workspace: board.workspace,
+    });
+  } else {
+    response.status(400);
+    throw new Error("Invalid request");
+  }
+});
 
 // @desc    Edit a board
-// @route   PUT //api/workspaces/:workspaceId/boards/:boardId
+// @route   PUT /api/workspaces/:workspaceId/boards/:boardId
 // @access  Private
-const editBoard = asyncHandler(async (request, res) => {
+const editBoard = asyncHandler(async (request, response) => {
     // To-do
  });
 
 // @desc    Delete a board
 // @route   DELETE /api/workspaces/:workspaceId/boards/:boardId
 // @access  Private
-const deleteBoard = asyncHandler(async (request, res) => {
+const deleteBoard = asyncHandler(async (request, response) => {
     // To-do
  });
 
 // @desc    Get a workspace's boards
 // @route   GET /api/workspaces/:workspaceId/boards/
 // @access  Private
-const getBoards = asyncHandler(async (request, res) => {
+const getBoards = asyncHandler(async (request, response) => {
   // To-do
 });
 
 // @desc    Get a workspace's board
-// @route   GET /api/workspaces/:workspaceId/boards/:id
+// @route   GET /api/workspaces/:workspaceId/boards/:boardId
 // @access  Private
-const getBoard = asyncHandler(async (request, res) => {
+const getBoard = asyncHandler(async (request, response) => {
    // To-do
 });
 
