a99d4c7ac5eada11ed9a71bf58ca9f4b58c1cfb9
sarahzacky <szacky10@gmail.com>
2023-10-23 19:53:36 -0700

    Update id to workspaceId in workspaceRoutes.js

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.route("/").get(protect, getWorkspaces).post(protect, createWorkspace);
 router
-  .route("/:id")
+  .route("/:workspaceId")
   .get(protect, getWorkspace)
   .put(protect, editWorkspace)
   .delete(protect, deleteWorkspace);
