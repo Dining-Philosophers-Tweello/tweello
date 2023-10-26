326b01e9dccf6fe8c45a260cf5e48c58fb0cebde
Diego O Garcia <diegogar577@gmail.com>
2023-10-26 12:55:14 -0700

    Revert "edit board"
    This reverts commit 2a9ac9425b7b93891e44a0d146813b2dbca92a07.

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import DeleteDialog from "@/components/delete-dialog";
-import EditBoardDialog from "@/components/edit-board-dialog";
 
 export default function Board() {
   const handleDelete = () => {
     console.log("Deleted board");
-  };
-  const handleEdit = () => {
-    console.log("Edited board");
   };
 
   return (
     <div className="flex flex-col gap-5 p-5 w-screen h-screen">
       <div className="flex flex-row gap-5">
         <div className="text-3xl">Insert board name here</div>
-        <EditBoardDialog componentName={"board"} handleEdit={handleEdit} />
         <DeleteDialog componentName={"Board"} handleDelete={handleDelete} />
       </div>
       <div className="flex gap-5 flex-wrap">Insert Columns Here</div>
