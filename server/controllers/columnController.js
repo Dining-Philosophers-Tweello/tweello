94ae5d38f234621bb071b122ece6ff4e8c5a0835
sarahzacky <szacky10@gmail.com>
2023-11-11 19:36:11 -0800

    Update getColumn method in columnController.js

--- server/controllers/columnController.js
+++ server/controllers/columnController.js
@@ -73,7 +73,59 @@ const deleteColumn = asyncHandler(async (request, response) => {});
 // @desc    Get a board's column
 // @route   GET /api/workspaces/:workspaceId/boards/:boardId/columns/:columnId
 // @access  Private
-const getColumn = asyncHandler(async (request, response) => {});
+const getColumn = asyncHandler(async (request, response) => {
+  const currentUserId = request.user._id;
+  const workspaceId = request.params.workspaceId;
+  const boardId = request.params.boardId;
+  const columnId = request.params.columnId;
+
+  // Check if the workspace exists
+  let workspace;
+  try {
+    workspace = await Workspace.findById(workspaceId);
+  } catch (error) {
+    response.status(404);
+    throw new Error("Workspace not found");
+  }
+  // Check against old deleted workspace IDs
+  if (!workspace) {
+    response.status(404);
+    throw new Error("Workspace not found");
+  }
+
+  // Check if the user has access to the workspace
+  if (
+    workspace.creator.toString() !== currentUserId.toString() &&
+    !workspace.members.includes(currentUserId)
+  ) {
+    response.status(403);
+    throw new Error("Unauthorized access to this workspace");
+  }
+
+  // Find the board
+  const board = workspace.boards.id(boardId);
+
+  // Check if the board exists
+  if (!board) {
+    response.status(404);
+    throw new Error("Board not found");
+  }
+
+  // Find the column
+  const column = board.columns.id(columnId);
+
+  // Check if the column exists
+  if (!column) {
+    response.status(404);
+    throw new Error("Column not found");
+  }
+
+  response.status(200).json({
+    _id: column._id,
+    name: column.name,
+    tasks: column.tasks,
+  });
+});
 
 // @desc    Get a board's columns
 // @route   GET /api/workspaces/:workspaceId/boards/:boardId/columns
