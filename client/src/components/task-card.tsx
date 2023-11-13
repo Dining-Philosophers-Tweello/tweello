9b22ac37947b3c6b19d4a4b3a8726c618adbb829
Diego O Garcia <diegogar577@gmail.com>
2023-11-13 09:15:53 -0800

    Visual bugs when dragging and hovering

--- client/src/components/task-card.tsx
+++ client/src/components/task-card.tsx
@@ -43,11 +43,10 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
       <Card
         ref={setNodeRef}
         style={style}
-        className=" min-h-[8rem] bg-card border-2 border-primary p-6 opacity-30"
+        className="  bg-card border-2 border-primary opacity-30 flex items-center flex-row justify-start min-h-[8rem]
+         w-full whitespace-pre-wrap max-h-32"
       >
-        <CardTitle className="flex justify-start pt-6">
-          {task.content}
-        </CardTitle>
+        <CardTitle className="flex justify-start p-6">{task.content}</CardTitle>
       </Card>
     );
   }
@@ -92,19 +91,19 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
       {...listeners}
     >
       <CardContent
-        className="flex items-center flex-row justify-between min-h-[6rem]
+        className="pt-6 flex items-center flex-row justify-between min-h-[6rem]
         my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap"
         onMouseEnter={() => setMouseIsOver(true)}
         onMouseLeave={() => setMouseIsOver(false)}
       >
-        <CardTitle className="justify-center flex items-center align-middle pt-6">
+        <CardTitle className="justify-center flex items-center align-middle">
           {task.content}
         </CardTitle>
         {mouseIsOver && (
           <Button
             variant="ghost"
             size="sm"
-            className="!mt-0 pt-6"
+            className=""
             onClick={() => deleteTask(task._id)}
           >
             <Trash2 size={20} />
